7ee6b49f398e8db8bc0e8c2818923a854ff97f01
Rhys Bridges <rhys_bridges@hotmail.com>
2018-06-20 16:40:17 +0100

    Added FormikApp HoC
    Also refactored App function into arrow function.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { withFormik } from 'formik';
+import Yup from 'yup';
 
 import "./styles.css";
 
-function App() {
-  return (
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <p>This is just a quick bit of content.</p>
+//       <div>Just a quick test.</div>
+//     </div>
+//   );
+// }
+
+const App = () => (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>This is just a quick bit of content.</p>
       <div>Just a quick test.</div>
     </div>
-  );
-}
+);
+
+const FormikApp = withFormik({
+
+})(App);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<FormikApp />, rootElement);
