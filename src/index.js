2d85183b40d735346400c887a223e1caabc28287
Rhys Bridges <rhys_bridges@hotmail.com>
2018-06-20 18:17:36 +0100

    Added a submit button
    Added bootstrap as a resource.

--- src/index.js
+++ src/index.js
@@ -21,14 +21,20 @@ const App = ({values, handleChange}) => (
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>This is just a quick bit of content.</p>
-      <div>
-        <label for='email'>Email address: </label>
-        <input type='email' id='email' name='email' placeholder='e.g. john@smith.com' value={values.email} onChange={handleChange}/>
-      </div>
-      <div>
-        <label for='password'>Password: </label>
-        <input type='password' id='password' name='password' placeholder='Your password' value={values.password} onChange={handleChange} />
-      </div>      
+
+      <form>
+        <div>
+          <label for='email'>Email address: </label>
+          <input type='email' id='email' name='email' placeholder='e.g. john@smith.com' value={values.email} onChange={handleChange}/>
+        </div>
+        <div>
+          <label for='password'>Password: </label>
+          <input type='password' id='password' name='password' placeholder='Your password' value={values.password} onChange={handleChange} />
+        </div>   
+        <div>
+          <button className='btn btn-primary'>Submit</button>
+        </div> 
+      </form>  
     </div>
 );
 
@@ -38,10 +44,10 @@ const FormikApp = withFormik({
       email: email || '',
       password: password || ''
     }
-  },
-  mapPropsToValues1: () => ({
-    email: 'some text'
-  })  
+  }
+  // mapPropsToValues1: () => ({
+  //   email: 'some text'
+  // })  
 })(App);
 
 const rootElement = document.getElementById("root");
