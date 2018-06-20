00efa57d03cf5e4e674c7b43e5b4161424e38f0b
Rhys Bridges <rhys_bridges@hotmail.com>
2018-06-20 20:02:10 +0100

    Added select and checkbox controls

--- src/index.js
+++ src/index.js
@@ -1,26 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { withFormik } from "formik";
+import { withFormik, Form, Field } from "formik";
 import Yup from "yup";
 
 import "./styles.css";
 
-const App = ({ values, handleChange, handleSubmit }) => (
+// const App = ({ values, handleChange, handleSubmit }) => (
+//   <div className="App">
+//     <h1>Hello CodeSandbox</h1>
+//     <h2>Start editing to see some magic happen!</h2>
+//     <p>This is just a quick bit of content.</p>
+
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label for="email">Email address: </label>
+//         <input
+//           type="email"
+//           id="email"
+//           name="email"
+//           placeholder="e.g. john@smith.com"
+//           value={values.email}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label for="password">Password: </label>
+//         <input
+//           type="password"
+//           id="password"
+//           name="password"
+//           placeholder="Your password"
+//           value={values.password}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <button className="btn btn-primary">Submit</button>
+//       </div>
+//     </form>
+//   </div>
+// );
+
+const App = ({ values, handleChange }) => (
   <div className="App">
     <h1>Hello CodeSandbox</h1>
     <h2>Start editing to see some magic happen!</h2>
     <p>This is just a quick bit of content.</p>
 
-    <form onSubmit={handleSubmit}>
+    <Form>
       <div>
         <label for="email">Email address: </label>
-        <input
+        <Field
           type="email"
           id="email"
           name="email"
           placeholder="e.g. john@smith.com"
-          value={values.email}
-          onChange={handleChange}
         />
       </div>
       <div>
@@ -30,26 +64,37 @@ const App = ({ values, handleChange, handleSubmit }) => (
           id="password"
           name="password"
           placeholder="Your password"
-          value={values.password}
-          onChange={handleChange}
         />
+      </div>
+      <div>
+        <label>
+          <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+          &nbsp;Join our newsletter
+        </label>
+      </div>
+      <div>
+        <Field component="select" name="plan">
+          <option value="free">Free</option>
+          <option value="premium">Premium</option>
+        </Field>
       </div>
       <div>
         <button className="btn btn-primary">Submit</button>
       </div>
-    </form>
+    </Form>
   </div>
 );
-
 const FormikApp = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       email: email || "",
-      password: password || ""
+      password: password || "",
+      newsletter: newsletter || true,
+      plan: plan || 'free'
     };
   },
   handleSubmit(values) {
-    alert("handling submit!");
+    // alert("handling submit!");
     console.log(values);
   }
   // mapPropsToValues1: () => ({
