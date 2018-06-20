94a343cd15975118c357d9495cf85d74731b96ef
Rhys Bridges <rhys_bridges@hotmail.com>
2018-06-20 17:01:07 +0100

    Provided initial values
    Linked to values property in wrapped component.

--- src/index.js
+++ src/index.js
@@ -16,19 +16,26 @@ import "./styles.css";
 //   );
 // }
 
-const App = () => (
+const App = ({values}) => (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>This is just a quick bit of content.</p>
       <div>
-        <input type='email' name='email' placeholder='e.g. john@smith.com'/>
+        <input type='email' name='email' placeholder='e.g. john@smith.com' value={values.email}/>
       </div>
     </div>
 );
 
 const FormikApp = withFormik({
-
+  mapPropsToValues() {
+    return {
+      email: 'test text'
+    }
+  },
+  mapPropsToValues1: () => ({
+    email: 'some text'
+  })  
 })(App);
 
 const rootElement = document.getElementById("root");
